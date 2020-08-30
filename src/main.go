66211595b3137cce2bfcee9131dd27780b689f0d
linbudu599 <linbudu599@gmail.com>
2020-08-30 21:41:44 +0800

    :whale: feat: basic syntax(0)

--- src/main.go
+++ src/main.go
@@ -3,5 +3,37 @@ package main
 import "fmt"
 
 func main() {
-   fmt.Println("Hello, World!")
+	var d bool = true
+	var a string = "linbudu"
+	var b, c int = 1, 2
+	e := false
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println("Hello, World!")
+
+	const (
+		Unknown = 0
+		Female  = 1
+		Male    = 2
+	)
+
+	fmt.Println(Unknown, Female, Male)
+
+	const (
+		aa = "abc" + "def"
+		bb = len(aa)
+		cc = unsafe.Sizeof(aa)
+   )
+
+   const (
+      ee = iota
+      dd
+      ff
+   )
+   
+
+
 }
